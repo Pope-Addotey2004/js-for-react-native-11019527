c10011e720c53d672fce4060ea87aeb09a6071c5
Reuben Addo <adoteythefirst17@gmail.com>
2024-05-18 11:56:34 +0100

    Task 1

---
+++ arrayManipulation.js
@@ -0,0 +1,15 @@
+function processArray(numbers) {
+  return numbers.map(number => {
+      if (number % 2 === 0) {
+          return number * number;
+      } else {
+          
+          return number * number * number;
+      }
+  });
+}
+
+// Example usage:
+const inputArray = [1, 2, 3, 4, 5];
+const resultArray = processArray(inputArray);
+console.log(resultArray); 
