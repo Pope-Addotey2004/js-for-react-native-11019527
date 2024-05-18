22a9205098c852de9da0720bf4a8105285ef4d1a
Reuben Addo <adoteythefirst17@gmail.com>
2024-05-18 12:35:50 +0100

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -10,6 +10,24 @@ function processArray(numbers) {
 }
 
 // Example usage:
-const inputArray = [1, 2, 3, 4, 5];
+const inputArray = [11,12,13,14,15];
 const resultArray = processArray(inputArray);
 console.log(resultArray); 
+
+function formatArrayStrings(strings, processedNumbers) {
+  return strings.map((str, index) => {
+      if (processedNumbers[index] % 2 === 0) {
+          return str.toUpperCase();
+      } else {
+          return str.toLowerCase();
+      }
+  });
+}
+
+// Example usage:
+const numbers = [1, 2, 3, 4, 5];
+const processedNumbers = processArray(numbers);
+const strings = ["Sir Paul", "Is a ", "Very", "Good", "Lecturer"];
+const formattedStrings = formatArrayStrings(strings, processedNumbers);
+
+console.log(formattedStrings);
